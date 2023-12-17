d8c55ed2ed0ef9ca025f07aed667891d92cc6db0
98Taek <imjt9345@naver.com>
2023-12-17 19:18:36 +0900

    Update) 환경변수 추가

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-txhsjld%m$=uvn*+xas%#2qr)oa%ixh=ul5ul*sp5@a*k25#y0"
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", "0")))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(filter(None, os.environ.get("ALLOWED_HOSTS", "").split(",")))
 
 
 # Application definition
